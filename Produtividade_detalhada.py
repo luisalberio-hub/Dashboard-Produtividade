4d7c25ccfe038dc12cc0beef1655e05eded6f139
luisalberio-hub <luis.alberio@gmail.com>
2026-02-20 19:47:13 -0300

    Clean up imports in Produtividade_detalhada.py
    Removed unused plotly imports and version check.

--- Produtividade_detalhada.py
+++ Produtividade_detalhada.py
@@ -1,10 +1,5 @@
-import plotly
-st.write(plotly.__version__)
-
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import io
 
 # --------------------------------
